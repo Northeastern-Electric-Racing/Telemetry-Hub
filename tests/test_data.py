5cd55b816f6b9d49dbc6e135aa57ba5c03ed3d32
rydzynskip <rydzynski.p@gmail.com>
2022-12-18 19:01:27 -0500

    Add script for generating test data

---
+++ tests/test_data.py
@@ -0,0 +1,20 @@
+"""
+This script generates a list of preprocessed data, taken from the test_data.csv file.
+Use the generate_data function to use in other parts of the code.
+Example usage:
+    from tests.test_data import generate_data
+    data = generate_data() # now have access to example data to test with
+"""
+
+import pandas as pd
+import numpy as np
+from typing import List, Tuple, Any
+
+def generate_data() -> List[Tuple[str, str, str]]:
+    """
+    Returns a list of data, each formatted as a tuple of the form (timestamp, id, value).
+    NOTE: You may have to update the file path for test_data.csv
+    """
+    data = pd.read_csv("test_data.csv")
+    data_array = np.delete(data.values, 2, axis=1)
+    return data_array.tolist()
