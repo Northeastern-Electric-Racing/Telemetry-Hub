a32c457202d409a3d63dccd4a96349a793c8279c
David <gunnerdog6@gmail.com>
2023-01-19 20:45:20 -0500

    Made Buttons Change Color Slightly When Pressed

--- ner_telhub/widgets/styled_widgets.py
+++ ner_telhub/widgets/styled_widgets.py
@@ -7,7 +7,7 @@ class NERButton(QPushButton):
     """
     Creates a button using a provided preset button style
     """
-
+        
     class Styles(Enum):
         """
         Defines the styles of buttons. When hovering over them, they turn two shades brighter.
@@ -15,13 +15,17 @@ class NERButton(QPushButton):
         """
         DEFAULT = ""
         RED = """QPushButton {color: white; background-color: #FF5656; border-radius: 4px; padding: 3% 8%;}
-            QPushButton:hover {background-color: #ff8080; color: white;}"""
+            QPushButton:hover {background-color: #ff8080; color: white;}
+            QPushButton::pressed {background-color : #fc9f9f; color: white;}"""
         GREEN = """QPushButton {color: white; background-color: #1a8f35; border-radius: 4px; padding: 3% 8%;}
-            QPushButton:hover {background-color: #20ac40; color: white;}"""
+            QPushButton:hover {background-color: #20ac40; color: white;}
+            QPushButton::pressed {background-color : #67ab76;}"""
         BLUE = """QPushButton {color: white; background-color: #0693E3; border-radius: 4px; padding: 3% 8%;}
-            QPushButton:hover {background-color: #1fa9f9; color: white;}"""
+            QPushButton:hover {background-color: #1fa9f9; color: white;}
+            QPushButton::pressed {background-color : #7dccfa;}"""
         GRAY = """QPushButton {color: white; background-color: #999999; border-radius: 4px; padding: 3% 8%;}
-            QPushButton:hover {background-color: #b3b3b3; color: white;}"""
+            QPushButton:hover {background-color: #b3b3b3; color: white;}
+            QPushButton::pressed {background-color : #c4c4c4;}"""
 
     def __init__(self, title: str, style=Styles.DEFAULT):
         super().__init__(title)
