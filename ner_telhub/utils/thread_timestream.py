92f58ed81cbe16f461aa2abe648b969cbdcdaebd
evanlombardo <evanlombardo@Evans-MacBook-Pro.local>
2023-03-02 16:39:26 -0500

    Added threading to timestream queries

---
+++ ner_telhub/utils/thread_timestream.py
@@ -0,0 +1,16 @@
+from PyQt6.QtCore import QDateTime
+
+from ner_processing.data import Data
+from ner_telhub.utils.timestream_constants import DATE_TIME_FORMAT
+
+def thread_timestream(row):
+    data = row['Data']
+
+    data_id = int(data[1]['ScalarValue'])
+    time = QDateTime.fromString(data[2]['ScalarValue'][:-6], DATE_TIME_FORMAT).toPyDateTime()
+    try:
+        value = float(data[3]['ScalarValue'])
+    except Exception:
+        value = data[4]['ScalarValue']
+
+    return Data(time, data_id, value)
